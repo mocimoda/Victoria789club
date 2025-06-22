bc10aee11cb651dd2dc348a09d51163483698ff2
Victoria789club <moci14807@gmail.com>
2025-06-22 19:30:54 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>ยินดีต้อนรับสู่ VICTORIA789.CLUB</h1>);
